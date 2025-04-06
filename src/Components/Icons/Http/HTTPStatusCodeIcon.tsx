1a7fbc2b3d6097f1b5a62d5820091e3173f3b5b9
Nikita Filonov <nikita.filonov@dif.tech>
2025-04-06 22:10:06 +0300

    Update HTTPStatusCodeIcon.tsx

--- src/Components/Icons/Http/HTTPStatusCodeIcon.tsx
+++ src/Components/Icons/Http/HTTPStatusCodeIcon.tsx
@@ -1,11 +1,13 @@
 import CircleIcon from '@mui/icons-material/Circle';
 import { getStatusCodeColor } from '../../../Services/Http/Utils';
 import { FC } from 'react';
+import { SxProps, Theme } from '@mui/material';
 
 type Props = {
+  sx?: SxProps<Theme>;
   code: number;
 };
 
-export const HTTPStatusCodeIcon: FC<Props> = ({ code }) => {
-  return <CircleIcon color={getStatusCodeColor(code)} />;
+export const HTTPStatusCodeIcon: FC<Props> = ({ sx, code }) => {
+  return <CircleIcon sx={sx} color={getStatusCodeColor(code)} />;
 };
