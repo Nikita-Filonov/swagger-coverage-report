403d0cb4be80983ee21bea8d5961dae6a9258e5b
Nikita Filonov <nikita.filonov@dif.tech>
2025-04-09 22:22:05 +0300

    Update ServiceEndpointStatusCodeCoverageTableRow.tsx

--- src/Components/Tables/Coverage/ServiceCoverage/ServiceEndpointStatusCodeCoverageTableRow.tsx
+++ src/Components/Tables/Coverage/ServiceCoverage/ServiceEndpointStatusCodeCoverageTableRow.tsx
@@ -1,8 +1,8 @@
 import { BaseTableRow } from '../../BaseTableRow';
 import { FC } from 'react';
 import { ServiceEndpointStatusCodeCoverage } from '../../../../Models/Coverage/ServiceCoverage';
-import { CoveredIcons } from '../../../Icons/Coverage/CoveredIcons';
 import { StatusCodeCell } from './StatusCodeCell';
+import { CoverageIcons } from '../../../Icons/Coverage/CoverageIcons';
 
 type Props = {
   code: ServiceEndpointStatusCodeCoverage;
@@ -13,7 +13,8 @@ export const ServiceEndpointStatusCodeCoverageTableRow: FC<Props> = ({ code }) =
     <BaseTableRow
       cells={[
         { value: <StatusCodeCell code={code.value} /> },
-        { value: <CoveredIcons covered={code.covered} /> },
+        { value: <CoverageIcons coverage={code.responseCoverage} /> },
+        { value: <CoverageIcons coverage={code.statusCodeCoverage} /> },
         { value: code.totalCases },
         { value: code.description }
       ]}
