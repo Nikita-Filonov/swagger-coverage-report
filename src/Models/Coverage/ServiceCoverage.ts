3b99172f1537635d23981e267512306632a46ef9
Nikita Filonov <nikita.filonov@dif.tech>
2025-04-09 22:21:21 +0300

    Update ServiceCoverage.ts

--- src/Models/Coverage/ServiceCoverage.ts
+++ src/Models/Coverage/ServiceCoverage.ts
@@ -1,21 +1,35 @@
 import { CoverageHistory } from '../History/CoverageHistory';
 import { HTTPMethod } from '../Http';
 
+export enum ServiceEndpointCoverageStatus {
+  Missing = 'MISSING',
+  Covered = 'COVERED',
+  Uncovered = 'UNCOVERED'
+}
+
 export interface ServiceEndpointStatusCodeCoverage {
   value: number;
-  covered: boolean;
   totalCases: number;
   description: string | null;
+  responseCoverage: ServiceEndpointCoverageStatus;
+  statusCodeCoverage: ServiceEndpointCoverageStatus;
+}
+
+export interface ServiceEndpointQueryParametersCoverage {
+  name: string;
+  coverage: ServiceEndpointCoverageStatus;
 }
 
 export interface ServiceEndpointCoverage {
   name: string;
   method: HTTPMethod;
   summary: string | null;
-  covered: boolean;
+  coverage: ServiceEndpointCoverageStatus;
   totalCases: number;
   statusCodes: ServiceEndpointStatusCodeCoverage[];
   totalCoverage: number;
+  queryParameters: ServiceEndpointQueryParametersCoverage[];
+  requestCoverage: ServiceEndpointCoverageStatus;
   totalCoverageHistory: CoverageHistory[];
 }
 
